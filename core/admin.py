875da9718ab52cdc95066026db34a71eb38dfaa8
AlexSilva7192 <alexsilva.iaw2018@gmail.com>
2019-09-30 19:44:38 -0300

    'segunda_versao'

--- core/admin.py
+++ core/admin.py
@@ -4,19 +4,24 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Contato
+from .models import *
 
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'email', 'telefone')
 
-admin.site.register(Contato, ContatoAdmin)
-
-
-from .models import Livro
-
-class LivroAdmin(admin.ModelAdmin):
+class LivrosAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'nome_do_autor', 'assunto')
 
-admin.site.register(Livro, LivroAdmin)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'matricula', 'data_nascimento')
 
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ('data_devolucao', 'data_retirada', 'aluno', 'devolvido')
+    filter_horizontal = ['livros']
 
+admin.site.register(Livros, LivrosAdmin)
+admin.site.register(Contato, ContatoAdmin)
+admin.site.register(Livro)
+admin.site.register(Autor)
+admin.site.register(Aluno, AlunoAdmin)
+admin.site.register(Emprestimo, EmprestimoAdmin)
